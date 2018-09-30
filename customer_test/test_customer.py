72fe2d158742f1e834a667f74bfe63c2d96cff18
Jayanta <jayantarcc.2007@gmail.com>
2018-09-30 14:13:11 +0530

    Get customer return 200

--- customer_test/test_customer.py
+++ customer_test/test_customer.py
@@ -5,7 +5,12 @@ from unittest.mock import patch
 
 class ApiTests(unittest.TestCase):
 
-    def test_getcustomer_returns_200(self):
+    @patch('customer.requests.get')
+
+    def test_getcustomer_returns_200(self, mock_get):
+
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.status = "OK"
         response = get_customer("0")
 
         # Assert that the request-response cycle completed successfully with status code 200.
